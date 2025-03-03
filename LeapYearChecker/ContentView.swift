8ff6aa008a52f396ee7bcfb662909723eaa649b4
Julien Hwang <jhwang@lcs.on.ca>
2025-03-03 14:20:11 -0500

    Initial Commit

---
+++ LeapYearChecker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LeapYearChecker
+//
+//  Created by Julien Hwang on 3/3/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LeapYearChecker/LeapYearCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeapYearCheckerApp.swift
+//  LeapYearChecker
+//
+//  Created by Julien Hwang on 3/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct LeapYearCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
