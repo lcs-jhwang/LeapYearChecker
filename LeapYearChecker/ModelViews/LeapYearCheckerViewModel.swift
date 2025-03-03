24709f23c3e03598f04f9a45cdaecbd9cdadaf04
Julien Hwang <jhwang@lcs.on.ca>
2025-03-03 14:50:24 -0500

    Made the view model for the app

---
+++ LeapYearChecker/ModelViews/LeapYearCheckerViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  LeapYearCheckerViewModel.swift
+//  LeapYearChecker
+//
+//  Created by Julien Hwang on 3/3/2025.
+//
+
+import Foundation
+
+@Observable
+class LeapYearCheckerViewModel {
+    //MARK: Stored properties
+    var providedYear: String
+    
+    var recoverySuggestion: String = ""
+    
+    var leapYearChecker: LeapYearChecker?{
+        guard let year = Int(providedYear) else {
+            recoverySuggestion = "Please provide a intergal value"
+            return nil
+        }
+        
+        recoverySuggestion = ""
+        
+        return LeapYearChecker(year: year)
+        
+    }
+    
+
+    
+    init(providedYear: String,
+         recoverySuggestion: String
+    ){
+        self.providedYear = providedYear
+        self.recoverySuggestion = recoverySuggestion
+    }
+}
